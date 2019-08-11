a20d5ab41e4a1e5ea313fcbd3b48dc2ef9323d36
sergeiudris <sergei.udris@gmail.com>
2019-08-11 16:52:19 +0300

    fimain1: foreign -libs

--- src/ui/figmain1/src/figmain1/core.cljs
+++ src/ui/figmain1/src/figmain1/core.cljs
@@ -1,11 +1,16 @@
 (ns figmain1.core
   (:require [clojure.repl :as repl]
             [probe.time.core :as pt]
-            [figmain1.hooks]))
+            [figmain1.hooks]
+            [react]
+            [react-dom]))
 
 (goog-define some-val 3)
 
 (.log js/console "Hello Live edit!!!!!")
+
+(.log js/console react)
+(.log js/console react-dom)
 
 (defn hi
   []
